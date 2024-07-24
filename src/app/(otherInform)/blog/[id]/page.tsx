3fb017214e702ecb9ff349de04bb4a66489a8948
Nadya afriyani azmi <nadyaafriyani45@gmail.com>
2024-07-24 23:25:16 +0800

    add blog details

---
+++ src/app/(otherInform)/blog/[id]/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import newsData from "../_db/newsData";
+
+interface NewsItem {
+  id: number;
+  title: string;
+  category: string;
+  description: string;
+  image: string;
+}
+
+const BlogDetail = ({ params }: { params: { id: string } }) => {
+  const id = parseInt(params.id, 10);
+  const newsItem = newsData.find((item) => item.id === id);
+
+  if (!newsItem) {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold">Blog tidak ditemukan</h1>
+        <p className="text-lg text-[#71747B] mt-2">
+          Maaf, data yang Anda cari tidak tersedia.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="pt-28 pb-16">
+      <Image
+        src={newsItem.image}
+        alt={newsItem.title}
+        className="w-full h-96"
+        width={1000}
+        height={300}
+      />
+      <div className="flex px-16 items-center justify-between">
+        <h1 className="text-3xl text-[#1A4D2E] font-bold mt-4">{newsItem.title}</h1>
+        <p className="bg-[#1A4D2E] px-8 py-2 text-sm text-[#FEF7FF] rounded-full mt-2">
+          {newsItem.category}
+        </p>
+      </div>
+      <div className="px-16">
+        <p className="text-lg text-[#71747B] mt-2">{newsItem.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export async function generateStaticParams() {
+  return newsData.map((item) => ({ id: item.id.toString() }));
+}
+
+export default BlogDetail;
--- src/app/(otherInform)/blog/_partials/cardSection.tsx
+++ src/app/(otherInform)/blog/_partials/cardSection.tsx
@@ -1,102 +1,14 @@
 'use client'
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import PaginationSection from "./paginationSection"; 
+import newsData from "../_db/newsData";
 
-function CardSection (){
-  const newsData = [
-    {
-      id: 1,
-      title: "Judul Berita 1",
-      category: "Kategori 1",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 2,
-      title: "Judul Berita 2",
-      category: "Kategori 2",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 3,
-      title: "Judul Berita 3",
-      category: "Kategori 3",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 4,
-      title: "Judul Berita 4",
-      category: "Kategori 4",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 5,
-      title: "Judul Berita 5",
-      category: "Kategori 5",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 6,
-      title: "Judul Berita 6",
-      category: "Kategori 6",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 7,
-      title: "Judul Berita 7",
-      category: "Kategori 7",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 8,
-      title: "Judul Berita 8",
-      category: "Kategori 8",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },,
-    {
-      id: 9,
-      title: "Judul Berita 9",
-      category: "Kategori 9",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 10,
-      title: "Judul Berita 10",
-      category: "Kategori 10",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-    {
-      id: 11,
-      title: "Judul Berita 11",
-      category: "Kategori 11",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus perspiciatis repellendus dolores incidunt culpa quibusdam eos dignissimos, aut harum sunt nihil optio nisi. Quasi voluptates eaque harum error cupiditate.",
-      image: "/aset/hero.png",
-    },
-  ];
-
+const CardSection = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 8;
+  const router = useRouter();
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -106,11 +18,14 @@ function CardSection (){
   const startIndex = (currentPage - 1) * itemsPerPage;
   const selectedItems = newsData.slice(startIndex, startIndex + itemsPerPage);
 
+  const handleReadMore = (id: number) => {
+    router.push(`/blog/${id}`);
+  };
+
   return (
     <>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 lg:px-8 md:px-6 px-4 gap-14">
         {selectedItems.map((item) => (
-          item && (
           <div
             key={item.id}
             className="max-w-sm bg-white border border-gray-200 rounded-lg shadow"
@@ -134,10 +49,15 @@ function CardSection (){
               <p className="mb-3 font-normal text-[#71747B] line-clamp-3">
                 {item.description}
               </p>
-              <h5 className="text-[#1A4D2E] text-xl">Lihat selengkapnya...</h5>
+              <h5
+                className="text-[#1A4D2E] text-xl cursor-pointer"
+                onClick={() => handleReadMore(item.id)}
+              >
+                Lihat selengkapnya...
+              </h5>
             </div>
           </div>
-        )))}
+        ))}
       </div>
       <PaginationSection
         currentPage={currentPage}
@@ -148,4 +68,4 @@ function CardSection (){
   );
 };
 
-export {CardSection};
+export { CardSection };
