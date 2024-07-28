51318124aeea18d614ba93ca4d0982df42c10cc7
Nadya afriyani azmi <nadyaafriyani45@gmail.com>
2024-07-28 11:15:09 +0800

    add new branch and add page struktur desa

---
+++ src/app/(otherInform)/strukturdesa/page.tsx
@@ -0,0 +1,13 @@
+function Page() {
+  return (
+    <div className="flex flex-col pt-28 items-center">
+      <div>
+        <h1 className="text-[#1A4D2E] text-4xl font-semibold">Struktur Perangkat Desa</h1>
+      </div>
+      <div>
+        <p>Card</p>
+      </div>
+    </div>
+  );
+}
+export default Page;
