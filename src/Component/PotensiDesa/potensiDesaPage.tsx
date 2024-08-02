3338bd0ac9e6c86a46206c352717529697e3b189
Moh.afizan <alfijan12@gmail.com>
2024-08-02 19:03:11 +0800

    update component PotensiDesaPage

--- src/Component/PotensiDesa/potensiDesaPage.tsx
+++ src/Component/PotensiDesa/potensiDesaPage.tsx
@@ -6,11 +6,13 @@ const PotensiDesa = () => {
     <div className="w-full h-full container mx-auto">
       <div className="mt-14 px-10 w-full h-full flex flex-col justify-start items-start">
         <h1 className="text-[#1A4D2E] text-[40px] font-bold">Potensi Desa</h1>
-        <p className="text-[20px]">
-          Menampilkan titik lokasi wisata yang ada di Desa Tosale
+        <p className="text-[20px] mb-10">
+          Secara umum Wilayah Desa Tosale memiliki Struktur tanah berpulut yang
+          cocok untuk perkebunan serta kawasan industri. Secara umum yang
+          menunjang untuk pengembangan Desa
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-y-8">
+      <div className="grid grid-cols-2">
         {Data &&
           Data.map((item) => (
             <CardPotensiDesa
